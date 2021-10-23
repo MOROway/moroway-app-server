d5ab8342b7bed924e04b021e14ed46bfe5ddbd25
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2021-10-23 17:27:20 +0200

    7.3

--- Globals.java
+++ Globals.java
@@ -3,13 +3,13 @@ package morowayAppTeamplay;
 import java.util.ArrayList;
 
 public class Globals {
-	
+
 	public static final ArrayList<Sessions> SESSIONS = new ArrayList<>();
     public static final ArrayList<Game> GAMES = new ArrayList<>();
-	
-	public static final double VERSION_CURRENT = 7.2;
-	public static final double VERSION_MIN = 7.2;
-	
+
+	public static final double VERSION_CURRENT = 7.3;
+	public static final double VERSION_MIN = 7.3;
+
 	public static final int ERROR_LEVEL_OKAY = 0;
 	public static final int ERROR_LEVEL_WARNING = 1;
 	public static final int ERROR_LEVEL_ERROR = 2;
