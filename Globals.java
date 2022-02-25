6a6ed49e0542fe1203fb0b474a030cf91a1b00dd
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2022-02-25 04:13:35 +0100

    7.4

--- Globals.java
+++ Globals.java
@@ -7,7 +7,7 @@ public class Globals {
 	public static final ArrayList<Sessions> SESSIONS = new ArrayList<>();
     public static final ArrayList<Game> GAMES = new ArrayList<>();
 
-	public static final double VERSION_CURRENT = 7.3;
+	public static final double VERSION_CURRENT = 7.4;
 	public static final double VERSION_MIN = 7.3;
 
 	public static final int ERROR_LEVEL_OKAY = 0;
